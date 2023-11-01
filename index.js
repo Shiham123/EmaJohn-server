da8df47bf724d75c8db5f625f36d6789a6726458
Shiham123 <s36651786@gmail.com>
2023-11-01 11:58:43 +0600

    pagination routing setup

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb://localhost:27017';
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-one.varjcyv.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = 'mongodb://localhost:27017';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-one.varjcyv.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,11 +20,9 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
+const run = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
+    // await client.connect();
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async (req, res) => {
@@ -34,17 +30,18 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
+    app.get('/productsCount', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+
     await client.db('admin').command({ ping: 1 });
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    console.log('You successfully connected to MongoDB!');
+  } catch (error) {
+    console.dir(error);
   }
-}
-run().catch(console.dir);
+};
+run();
 
 app.get('/', (req, res) => {
   res.send('john is busy shopping');
